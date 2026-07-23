ce099edf6428d73ada4b882f412d841e64b0bb16
TheTurboForge <288388994+TheTurboForge@users.noreply.github.com>
2026-07-23 12:47:35 +0200

    Record credential key derivation provenance
    Signed-off-by: TheTurboForge <288388994+TheTurboForge@users.noreply.github.com>

--- components/gvmd/src/credential_key.c
+++ components/gvmd/src/credential_key.c
@@ -1,7 +1,8 @@
-/* Copyright (C) 2009-2022 Greenbone AG
- * YAFVS modifications Copyright (C) 2026 Robert Pelfrey <Robert@Pelfrey.de>.
- *
+/* SPDX-FileCopyrightText: 2009-2022 Greenbone AG
+ * SPDX-FileCopyrightText: 2026 Robert Pelfrey <robert@pelfrey.de>
  * SPDX-License-Identifier: AGPL-3.0-or-later
+ * YAFVS-Derivation: adaptation
+ * YAFVS-Source-Provenance: greenbone/gvmd commit 39a51f6ca6ad3d9383765436d2695d87e7dd8933, src/lsc_user.c and src/lsc_user.h, AGPL-3.0-or-later
  */
 
 /**
--- components/gvmd/src/credential_key_tests.c
+++ components/gvmd/src/credential_key_tests.c
@@ -1,6 +1,7 @@
 /* SPDX-FileCopyrightText: 2026 Robert Pelfrey <Robert@Pelfrey.de>
  *
  * SPDX-License-Identifier: AGPL-3.0-or-later
+ * YAFVS-Derivation: original
  */
 
 #include "credential_key.c"
